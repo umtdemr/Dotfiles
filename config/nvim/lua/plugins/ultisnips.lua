71f4f472c1be4e2afb5770613041f1cd2eb8c5b3
umtdemr <umitde692@gmail.com>
2022-02-21 15:13:52 +0300

    Deleted old unusing dotfiles

--- config/nvim/lua/plugins/ultisnips.lua
+++
@@ -1,4 +0,0 @@
-vim.g.UltiSnipsExpandTrigger            = '<C-l>.'
-vim.g.UltiSnipsJumpForwardTrigger       = '<C-j>'
-vim.g.UltiSnipsJumpBackwardTrigger      = '<C-k>'
-vim.g.UltiSnipsRemoveSelectModeMappings = 0
