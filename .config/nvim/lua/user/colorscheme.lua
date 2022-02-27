ef9a46de10b053661e6d477e576b1c33eaeb37ea
umtdemr <umitde692@gmail.com>
2022-02-27 14:01:58 +0300

    secure importing for color scheme

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,8 @@
-local catppuccin = require("catppuccin")
+local status_ok, catppuccin = pcall(require, "catppuccin")
+if not status_ok then
+  return
+end
+
 catppuccin.setup({
   transparent_background = true,
 })
