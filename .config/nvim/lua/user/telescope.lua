0d7a84dcad5f3add4349f21cec96739c39f4d4ac
umtdemr <umitde692@gmail.com>
2022-02-27 13:18:54 +0300

    added telescope file_ignore_patterns for excluding node_modules 🤟

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -11,6 +11,7 @@ telescope.setup {
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
+    file_ignore_patterns = { "node_modules" },
 
     mappings = {
       i = {
