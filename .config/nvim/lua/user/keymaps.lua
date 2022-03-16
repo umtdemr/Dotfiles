fe4b340c27c435b75599c4db44f90b42066544fd
umtdemr <umitde692@gmail.com>
2022-03-16 23:03:26 +0300

    keymaps configured for splitting window

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -60,10 +60,15 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
+
+-- Splitting window --
+keymap("n", "sv", ":vsplit<CR>", opts)  -- Split vertical
+keymap("n", "sh", ":vsplit<CR>", opts)  -- Split horizontal
+
 -- Terminal --
 -- Better terminal navigation
 -- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
+  -- Split horizontal
