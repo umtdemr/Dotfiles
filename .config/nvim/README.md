78b36a07236d7d319844f15f8115e525f1d2c77c
umtdemr <umitde692@gmail.com>
2025-07-06 19:06:57 +0300

    fix: sub neovim folder

---
+++ .config/nvim/README.md
@@ -0,0 +1,9 @@
+# Neovim
+
+This is the neovim configuration I use. I gave up on configuring neovim myself. Instead, I've started to use [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim).
+
+## Screenshots
+
+![Neovim code](../../assets/neovim.png)
+![neovim telescope find files preview](../../assets/neovim-telescope.png)
+
---
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,26 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
